4575b50f9d787f782387ebe4bf4b61020def625b
panmenglin <msx.pan@outlook.com>
2021-12-02 18:38:52 +0800

    chore: publish 1.2.2

--- types/eval-scripts.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * 改变作用域链 执行子组件 js
- * @param code
- * @returns
- */
-export declare const createEvalScripts: (codes: string[]) => any;
--- types/type.d.ts
+++
@@ -1,7 +0,0 @@
-export declare type TenonBlock = {
-    key: string;
-    import: string;
-    name: string;
-    domain: string;
-    props?: Record<string, string>;
-};
